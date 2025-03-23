595e6091a10e810c36c0a766cb7e16cfc0e25980
ambdmatap <40763481-ambdmatap@users.noreply.replit.com>
2025-03-23 18:52:09 +0000

    Restored to 'b82a314d5cfb99a1b7d527a979677fa3563d4d9f'
    Replit-Restored-To: b82a314d5cfb99a1b7d527a979677fa3563d4d9f

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/Navbar";
-import ScrollToTop from "@/components/ScrollToTop";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import DestinationsPage from "@/pages/destinations";
@@ -29,7 +28,6 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <div className="font-body text-neutral-dark bg-neutral-light min-h-screen">
         <Router />
-        <ScrollToTop />
         <Toaster />
       </div>
     </QueryClientProvider>
